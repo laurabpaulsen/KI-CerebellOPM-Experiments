e442b9e480e60e6f44ae8457ad9a73ef6152530d
laurabpaulsen <202005791@post.au.dk>
2026-01-26 16:01:24 +0100

    update trigger test script

--- tests/trig_test.py
+++ tests/trig_test.py
@@ -3,14 +3,11 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parents[1]))
 
-from utils.triggers_nidaqmx import setParallelData, create_trigger_mapping
+from utils.triggers_nidaqmx import setParallelData, 
 
-print("Loading trigger mapping...")
-mapping = create_trigger_mapping()
 
-print("Starting trigger test...\n")
 
-# List of keys you want to test
+# keys to test
 keys_to_test = [
     "stim/salient",
     "target/middle",
@@ -23,10 +20,31 @@ keys_to_test = [
     "experiment/end",
 ]
 
+def create_trigger_mapping( stim = 1, target = 2, middle = 4, index = 8,response = 16, correct = 32, incorrect = 64):
+    trigger_mapping = {
+        "stim/salient": stim,
+        "target/middle": target + middle,
+        "target/index": target + index,
+        "response/index/correct": response + index + correct,
+        "response/middle/incorrect": response + middle + incorrect,
+        "response/middle/correct": response + middle + correct,
+        "response/index/incorrect": response + index + incorrect,
+        "break/start": 128,
+        "break/end": 129,            
+        "experiment/start": 254,
+        "experiment/end": 255
+        }
+
+    return trigger_mapping
+        
+
+mapping = create_trigger_mapping()
+
+print("Starting trigger test...\n")
 for key in keys_to_test:
     bits = mapping[key]
     print(f"Sending trigger: {key} -> {bits}")
     setParallelData(bits)
     time.sleep(0.5)
 
-print("\nDone. If everything is wired correctly, you should see all of these pulses.")
+print("\nTrigger test completed.")
