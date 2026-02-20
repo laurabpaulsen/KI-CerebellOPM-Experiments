d2da53498ada37ffdd1ce5ad896e0037c5cc1128
laurabpaulsen <202005791@post.au.dk>
2026-02-20 11:40:11 +0100

    init script for lowering lines of a list of ports

---
+++ reset_ports.py
@@ -0,0 +1,44 @@
+"""
+This script is used to reset the lines of the specified ports to 0. This can be useful if the lines were left in a high state after an experiment, which can cause issues with subsequent experiments that expect the lines to be in a low state at the start.
+"""
+
+# -*- coding: utf-8 -*-
+
+
+import nidaqmx
+from nidaqmx.constants import LineGrouping
+
+PORTS = ["Dev1/port9/line0:7", "Dev1/port0/line0:7", "Dev1/port3/line0:7"]  # All 8 lines of port 0, 3, 9
+
+def init_tasks(PORTS):
+    
+    tasks = []
+    
+    for port in PORTS:
+        task = nidaqmx.Task()
+        task.do_channels.add_do_chan(
+            port,
+            line_grouping=LineGrouping.CHAN_FOR_ALL_LINES
+        )
+        tasks.append(task)
+
+    return tasks
+
+        
+def lower_lines():
+    tasks = init_tasks()
+
+    for task in tasks:
+        task.write(0, auto_start=True)  # Set lines to desired code without starting yet 
+   
+
+def close_tasks():
+    tasks = init_tasks()
+
+    for task in tasks:
+        task.close()
+
+if __name__ == "__main__":
+    lower_lines()
+    close_tasks()
+  
