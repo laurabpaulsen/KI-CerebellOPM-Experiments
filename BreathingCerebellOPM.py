63adf18147a309564f0e9e5f084dc9ac7c292011
laurabpaulsen <202005791@post.au.dk>
2026-04-01 14:13:29 +0200

    change way of making breaks

--- BreathingCerebellOPM.py
+++ BreathingCerebellOPM.py
@@ -559,15 +559,24 @@ def generate_block_order(ISIs: List[float], n_repeats: int) -> List[int]:
         available_start_blocks.remove(start_block)
 
         tmp_order = build_block_order(wanted_transitions, start_blocks=[start_block])
-        #order.extend(tmp_order)
+        order.extend(tmp_order)
         #if i != n_repeats - 1:
         #    order.append("break")
-        for idx, b in enumerate(tmp_order):
-            order.append(b)
-            if (idx + 1) % 5 == 0:  # add a break every 5 blocks
-                order.append("break")
+    
+    # insert breaks every 6 blocks
+    order_with_breaks = []
+    for idx, block in enumerate(order):
+        order_with_breaks.append(block)
+        if (idx + 1) % 6 == 0 and (idx + 1) != len(order):
+            order_with_breaks.append("break")
+    return order_with_breaks
 
-    return order
+    #for idx, b in enumerate(tmp_order):
+    #        order.append(b)
+    #        if (idx + 1) % 5 == 0:  # add a break every 5 blocks
+    #            order.append("break")
+
+    #return order
 
 
 # UTILITIES
